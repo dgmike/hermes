56423312ca0edd71a0d3ea9b177d02dc83bbf950
Michael Granados <michaelgranados@gmail.com>
2021-04-16 12:06:44 -0300

    feat: created a simple server app
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export default {};
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => res.json({ result: 'OK' }));
+
+app.listen(process.env['PORT'] || 3000, () => console.log('Listen on port 3000'));
