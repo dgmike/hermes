9c0f45c5f7cacf8a2d84d9e012b26ddeea0ce2ff
Michael Granados <michaelgranados@gmail.com>
2021-04-20 19:51:59 -0300

    feat: add swagger.yaml route, filter -able models
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { validationMetadatasToSchemas } from "class-validator-jsonschema";
-import { Application, Request, Response } from "express";
+import { Application, Response } from "express";
 import jwt from "jsonwebtoken";
 import {
   Action,
@@ -8,12 +8,13 @@ import {
 } from "routing-controllers";
 import { routingControllersToSpec } from "routing-controllers-openapi";
 import swaggerUi from "swagger-ui-express";
+import { stringify } from "yaml";
 import database from "./configurations/database";
 import {
   AuthenticationController,
+  ClientsController,
   RootController,
   UsersController,
-  ClientsController,
 } from "./controllers";
 
 export const createApp = async (): Promise<Application> => {
@@ -43,9 +44,22 @@ export const createApp = async (): Promise<Application> => {
   });
 
   const storage = getMetadataArgsStorage();
-  const schemas = validationMetadatasToSchemas({
-    refPointerPrefix: "#/components/schemas/",
-  });
+  const originalSchemas = validationMetadatasToSchemas();
+
+  const schemas = Object.entries(originalSchemas)
+    .filter(([key]: [string, unknown]): boolean => !key.includes("ableModel"))
+    .reduce(
+      (
+        acc: { [key: string]: unknown },
+        [key, value]: [string, unknown]
+      ): // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      { [key: string]: any } => ({
+        ...acc,
+        [key]: value,
+      }),
+      {}
+    );
+
   const swaggerSpec = routingControllersToSpec(
     storage,
     {},
@@ -64,8 +78,12 @@ export const createApp = async (): Promise<Application> => {
 
   app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-  app.get("/api/swagger", (_req: Request, res: Response): void => {
+  app.get("/api/swagger(.json)?", (_req, res: Response): void => {
     res.status(200).json(swaggerSpec);
+  });
+
+  app.get("/api/swagger.ya?ml", (_req, res: Response) => {
+    res.status(200).type("text/yaml").send(stringify(swaggerSpec));
   });
 
   app.locals["db"] = await database();
