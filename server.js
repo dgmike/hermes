4368c2057949caec151cd7c9ee448c3d177bf180
Michael Castillo Granados <michaelgranados@gmail.com>
2021-06-18 04:38:44 -0300

    feat: tracing listen error
    Signed-off-by: Michael Castillo Granados <michaelgranados@gmail.com>

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const createApp = require('./app');
 const port = parseInt(process.env.PORT || '3000');
 
 createApp()
-  .then((app) => 
+  .then((app) =>
     app.listen(port, () => console.log(`listen on port ${port}`))
-  );
+  )
+  .catch((err) => {
+    console.log('Erro ao levantar a aplicacao:', err)
+  });
