2403c5c24a67057645b8b949b085dc5fdca88551
Michael Castillo Granados <michaelgranados@gmail.com>
2021-06-24 01:32:50 -0300

    feat: added route for oauth2
    Signed-off-by: Michael Castillo Granados <michaelgranados@gmail.com>

--- routes.js
+++ routes.js
@@ -79,7 +79,7 @@ router.use(
 router.get("/oauth", async (req, res) => {
   const { db } = req.app.locals;
   const integration = await db("integrations")
-    .where({ integration_id: `${req.query.client_id}` })
+    .where({ client_id: `${req.query.client_id}` })
     .first();
 
   const redirect = new URL(integration.authorization_url);
@@ -88,8 +88,9 @@ router.get("/oauth", async (req, res) => {
   redirect.searchParams.append(
     "redirect_uri",
     // "https://script.google.com/macros/d/1uaDCN3bQKVJLCJAGUOmE6bof_jv8E0i_wQhrI9p0zGgH9sXSQDXOKeqy/usercallback"
-    "http://localhost:3000/oauth_callback",
+    // "http://localhost:3000/oauth_callback",
     // req.query.redirect_uri,
+    'https://hermes-store.herokuapp.com/oauth2'
   );
   // redirect.searchParams.append('state', req.query.state);
   redirect.searchParams.append("state", "some-random-value-from-database");
@@ -112,6 +113,12 @@ router.get("/oauth", async (req, res) => {
       searchParams: Object.fromEntries([...redirect.searchParams.entries()]),
       username: redirect.username,
     },
+  });
+});
+
+router.get("/oauth2", async (req, res) => {
+  res.json({
+    params: req.params,
   });
 });
 
