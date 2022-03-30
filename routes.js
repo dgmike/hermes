26271a157df1beb9daf8914cbd8f6bcd4ff79d79
Michael <contato@dgmike.com.br>
2022-03-30 07:31:30 -0300

    typo

--- routes.js
+++ routes.js
@@ -82,7 +82,7 @@ router.get("/oauth2", oauthController.oauth2);
 router.post("/token", oauthController.token);
 
 router.post("/callback", (req, res) => {
-  console.ingo('/callback', { body: req.body, params: req.params, query: req.query });
+  console.info('/callback', { body: req.body, params: req.params, query: req.query });
   res.json({ ok: true });
 })
 
